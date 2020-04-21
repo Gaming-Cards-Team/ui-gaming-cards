66470cb76f2ba7af8ea22f930821c1d8a3ed2beb
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-04-21 15:21:22 -0500

    Card-76 [@twjmgorduez, @daviddavilatw, @cristtynu] Switch material-ui to react-botstrap - Update header

--- src/main/components/Header.js
+++ src/main/components/Header.js
@@ -1,34 +1,20 @@
-import AppBar from "@material-ui/core/AppBar";
-import { makeStyles } from "@material-ui/core/styles";
-import Toolbar from "@material-ui/core/Toolbar";
 import React from "react";
 import logo from "../resources/logo.png"
+import Navbar from 'react-bootstrap/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1
-  },
-  logo: {
-    width: 135,
-    height: 43.54
-  }
-});
 
 const Header = () => {
-  const classes = useStyles();
   return (
-    <nav className={classes.root} id='header'>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <img
-            data-testid='logo'
-            className={classes.logo}
-            src={logo}
-            alt="Level Up Logo" 
-          />
-        </Toolbar>
-      </AppBar>
-    </nav>
+     <Navbar bg="light" variant="light">
+        <Navbar.Brand href="#home">
+          <img data-testid='logo'
+              className=''
+              height='48'
+              src={logo}
+              alt="Level Up Logo" />
+        </Navbar.Brand>
+      </Navbar>
   );
 };
 
