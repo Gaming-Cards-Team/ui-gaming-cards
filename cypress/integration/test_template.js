f95c40e9a24742ce9a60f31ff0e34075bd506aeb
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-04-21 15:22:11 -0500

    Card-76 [@twjmgorduez, @daviddavilatw, @cristtynu] Switch material-ui to react-botstrap - Update header

--- cypress/integration/test_template.js
+++ cypress/integration/test_template.js
@@ -3,8 +3,5 @@ describe('Shows the template page', () => {
   it('Should displays 3 cards', () => {
       cy.visit('http://localhost:3000');
       
-      cy.get('[id="header"]');
-      cy.contains('Body');
-      cy.contains('Footer');
   });
 });
