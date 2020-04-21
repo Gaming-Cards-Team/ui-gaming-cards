7f8f677de1c30ebdeb0a8cdac2660c4e3b346b4d
David Davila <david.davila@thoughtworks.com>
2020-04-21 10:29:42 -0500

    Fixs the test

--- cypress/integration/test_template.js
+++ cypress/integration/test_template.js
@@ -2,7 +2,7 @@ describe('Shows the template page', () => {
     it('Should displays 3 cards', () => {
       cy.visit('http://localhost:3000');
       
-      cy.contains('Header');
+      cy.get('[id="header"]');
       cy.contains('Body');
       cy.contains('Footer');
     });
