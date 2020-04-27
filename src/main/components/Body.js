626156c63bede72a2e772aee53ed9df2af4f698f
David Davila <david.davila@thoughtworks.com>
2020-04-27 12:33:51 -0500

    Card-77 [@daviddavilatw, @cristtynu] Fixs banner with the last changes

--- src/main/components/Body.js
+++ src/main/components/Body.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "./Banner";
+import TopMessage from "./TopMessage";
 import CardGallery from "./CardGallery";
 
 const Body = () => {
   return (
     <div>
       <Banner></Banner>
+      <TopMessage></TopMessage>
      
       <CardGallery></CardGallery>
     </div>
--- src/main/util/constant.js
+++ src/main/util/constant.js
@@ -1,3 +1,3 @@
-let year = new Date().getFullYear();
-export const FOOTER_TEXT = `Star Wars ${year} All rights reserved. Copyright Level Up `;
+export const FOOTER_TEXT = `Copyright Star Wars and all associated names and/or images are copyright Lucasfilm Ltd.`;
 export const BANNER_ALT = `Star Wars Banner`;
+export const MESSAGE_FOR_OTHER_CARDS = `Are you looking for cards for other game cards? check our catalog here`;
