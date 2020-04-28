c2d4f8097009391c3b1d3c05ab2f821d4379fd64
cristtynu <35372981+cristtynu@users.noreply.github.com>
2020-04-28 08:05:51 -0500

    Card-79 [@daviddavilatw, @cristtynu] Make responsive the gallery

--- src/components/CardGallery.js
+++ src/components/CardGallery.js
@@ -1,7 +1,9 @@
 import React from "react";
 import CardDeck from "react-bootstrap/CardDeck";
-import ProductCard from "./ProductCard"
-import mockGetCards from "./__mocks__/mockGetCards"
+import ProductCard from "./ProductCard";
+import mockGetCards from "./__mocks__/mockGetCards";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row"
 
 const cardGalleryStyle = {
   marginTop: 30,
@@ -11,19 +13,23 @@ const cardGalleryStyle = {
 };
 
 const CardGallery = () => {
-  const {data} = mockGetCards;
-return ( <div style={cardGalleryStyle}>
-   
-    <CardDeck> 
-      {data.map(card => (
-              <ProductCard 
+  const { data } = mockGetCards;
+  return (
+    <div style={cardGalleryStyle}>
+      <CardDeck className='h-auto'>
+        {data.map((card) => (
+          <Col lg={3} md={4} sm={6} xs={12} >
+            <ProductCard
               image={card.image}
               name={card.name}
-              price={"$1.00"} >
-              </ProductCard>
-      ))}
-    </CardDeck>
-  </div>
-)};
+              price={"$1.00"}
+            ></ProductCard>
+            
+          </Col>
+        ))}
+      </CardDeck>
+    </div>
+  );
+};
 
 export default CardGallery;
