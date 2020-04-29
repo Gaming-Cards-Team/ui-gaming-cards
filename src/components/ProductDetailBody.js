d61f74b1bf41c6010fc91005a7706daa96804cbc
cristtynu <35372981+cristtynu@users.noreply.github.com>
2020-04-29 12:04:09 -0500

    Card-82 [@daviddavilatw, @cristtynu] Create Detail for Product Card

--- src/components/ProductDetailBody.js
+++ src/components/ProductDetailBody.js
@@ -3,28 +3,23 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ProductText from "./ProductText";
 
-
-const imageStyle = {
-  textAlign: "right",
-};
-const productTextStyle = {
-  color: "#2d963f",
-  fontWeight: "bold",
-};
-
 const ProductDetailBody = () => {
   return (
     <div>
-    <Row>
-      <Col xs={5} style={imageStyle}>
-        Imagen
-      </Col>
-      <Col xs={7} style={productTextStyle}>
-            <ProductText />
-      </Col>
-    </Row>
-  </div>
-
+      <Row>
+        <Col lg={1} md={0} sm={12} xs={12}></Col>
+        <Col lg={4} md={5} sm={12} xs={12}>
+          <img
+            src="https://swdestinydb.com/bundles/cards/en/12/12081.jpg"
+            alt="Card"
+          />
+        </Col>
+        <Col lg={6} md={7} sm={12} xs={12}>
+          <ProductText />
+        </Col>
+        <Col lg={1} md={0} sm={12} xs={12}></Col>
+      </Row>
+    </div>
   );
 };
 
---
+++ src/util/cardTextConstant.js
@@ -0,0 +1,8 @@
+export const RARITY = `Rarity: `;
+export const CARD = `Card#: `;
+export const CARD_TYPE = `Card type: `;
+export const AFFILIATION = `Affiliation: `;
+export const COLOR = `Color: `;
+export const POINT_VALUES = `Point value(s): `;
+export const UNIQUENESS = `Uniqueness: `;
+export const DESCRIPTION = `Description: `;
