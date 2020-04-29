f269aeb52e01f765c6b724cacdcba1ff6ea485b9
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-04-29 18:07:55 -0500

    NO-STORY [@twjmgorduez] Adds a step to the pipeline to check the app deployment PRO

--- cypress/integration/testDeployPRO.ispec.js
+++ cypress/integration/testDeployPRO.ispec.js
@@ -1,6 +1,6 @@
 describe('Deployment checker', () => {
     
-  it('Should get the component ', () => {
+  it('Should get the mainPage component', () => {
       cy.visit('http://ec2-3-136-158-140.us-east-2.compute.amazonaws.com/');
       
       cy.get('[data-testid="mainPage"]')
