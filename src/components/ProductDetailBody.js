392989415692dd4b12afc9360fe4f699f4bb304d
David Davila <david.davila@thoughtworks.com>
2020-04-29 12:25:27 -0500

    Card-82 [@daviddavilatw, @cristtynu] Changes padding to improve responsive visualization

--- src/components/ProductDetailBody.js
+++ src/components/ProductDetailBody.js
@@ -2,22 +2,31 @@ import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import ProductText from "./ProductText";
+const imageStyle = {
+  textAlign: "center",
+  padding: "40px 0px",
+};
+const productTextStyle = {
+  padding: "40px",
+};
 
 const ProductDetailBody = () => {
   return (
     <div>
       <Row>
-        <Col lg={1} md={0} sm={12} xs={12}></Col>
-        <Col lg={4} md={5} sm={12} xs={12}>
+        <Col lg={2} md={1} sm={12} xs={12}></Col>
+        <Col lg={3} md={4} sm={12} xs={12} style={imageStyle}>
           <img
             src="https://swdestinydb.com/bundles/cards/en/12/12081.jpg"
             alt="Card"
           />
         </Col>
-        <Col lg={6} md={7} sm={12} xs={12}>
+        <Col lg={6} md={6} sm={12} xs={12}>
+          <div style={productTextStyle}>
           <ProductText />
+          </div>
         </Col>
-        <Col lg={1} md={0} sm={12} xs={12}></Col>
+        <Col lg={1} md={1} sm={12} xs={12}></Col>
       </Row>
     </div>
   );
