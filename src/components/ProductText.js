78cc9959c1169e0e4fd84b7ec1df81f410e8a553
cristtynu <35372981+cristtynu@users.noreply.github.com>
2020-04-29 12:04:09 -0500

    Card-82 [@daviddavilatw, @cristtynu] Create Detail for Product Card

--- src/components/ProductText.js
+++ src/components/ProductText.js
@@ -1,26 +1,51 @@
 import React from "react";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import {RARITY, CARD,CARD_TYPE,AFFILIATION,COLOR,POINT_VALUES,UNIQUENESS,DESCRIPTION} from "../util/cardTextConstant";
 
-const attributesStyle = {
-  textAlign: "right",
+const cardStyle = {
+  paddingTop: 10,
+  paddingBottom: 15,
 };
+
+const attributesStyle = {};
 const descriptionStyle = {
   color: "#2d963f",
   fontWeight: "bold",
 };
-
+const titleStyle = {
+  color: "#2d963f",
+  fontSize: "xx-large",
+};
+const subtitleStyle = {
+  fontWeight: "bold",
+}
 const ProductText = () => (
   <div>
-    Nombre de la tarjeta Mini detalle
-    <Row>
-      <Col xs={6} style={attributesStyle}>
-        Atributos
-      </Col>
-      <Col xs={6} style={descriptionStyle}>
-        Desripcion
-      </Col>
-    </Row>
+    <span style={titleStyle}> Destroy the Death Start</span>
+    <p>Covert Missions</p>
+    <div style={cardStyle}>
+      <Card>
+        <Card.Body>
+          <Row>
+            <Col xs={6} style={attributesStyle}>
+              <p><span style={subtitleStyle}>{RARITY}</span>Uncommon</p>
+              <p><span style={subtitleStyle}>{CARD}</span>81</p>
+              <p><span style={subtitleStyle}>{CARD_TYPE}</span>Plot</p>
+              <p><span style={subtitleStyle}>{AFFILIATION}</span>Hero</p>
+              <p><span style={subtitleStyle}>{COLOR}</span>Red</p>
+              <p><span style={subtitleStyle}>{POINT_VALUES}</span>2</p>
+              <p><span style={subtitleStyle}>{UNIQUENESS}</span>Non/Unique</p>
+            </Col>
+            <Col xs={6}>
+            <p style={subtitleStyle}>{DESCRIPTION}</p>
+            After setup, give this plot 20 shields. You can resolve your <i>pilot</i> and <i>vehicle</i> dice showing ranged damage against this plot. <b>Action</b> - If this plot has no shields, spend 1 resource to reroll 1 of your <i>pilot</i> dice and 1 of your <i>vehicle</i> dice. Then, if both of those dice are showing damage, you win the game.
+            </Col>
+          </Row>
+        </Card.Body>
+      </Card>
+    </div>
   </div>
 );
 
