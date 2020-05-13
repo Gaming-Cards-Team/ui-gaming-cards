842fbba3cba9cf9f718cf041a93bbb9fbdb23703
”JuanMa” <juanmanuel.garcia@thoughtworks.com>
2020-05-12 19:43:28 -0500

    NO-STORY [@twjmgorduez] Updates pipeline

--- cypress/integration/testDeployQA.ispec.js
+++ cypress/integration/testDeployQA.ispec.js
@@ -1,7 +1,7 @@
 describe('Deployment checker', () => {
     
   it('Should get the mainPage component', () => {
-      cy.visit('http://ec2-3-15-56-101.us-east-2.compute.amazonaws.com/');
+      cy.visit('http://ec2-3-23-123-190.us-east-2.compute.amazonaws.com/');
       
       cy.get('[data-cy="mainPage"]').should('exist');
   });
